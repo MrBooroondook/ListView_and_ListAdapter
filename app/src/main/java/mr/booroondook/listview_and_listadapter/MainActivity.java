dcf4b1e8abd0a9a4f2166255144fc95fd3f1483a
Mr. Booroondook <Mr.Booroondook@gmail.com>
2020-04-16 16:35:51 +0400

    Added ListView and ListAdapter

--- app/src/main/java/mr/booroondook/listview_and_listadapter/MainActivity.java
+++ app/src/main/java/mr/booroondook/listview_and_listadapter/MainActivity.java
@@ -3,12 +3,33 @@ package mr.booroondook.listview_and_listadapter;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final String[] lastNames = getResources().getStringArray(R.array.last_names);
+        ListView listView = findViewById(R.id.list_view);
+        textView = findViewById(R.id.text_view);
+
+        listView.setAdapter(new ArrayAdapter<>(this,
+                android.R.layout.simple_list_item_1,
+                lastNames));
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                textView.setText(lastNames[position]);
+            }
+        });
     }
 }
